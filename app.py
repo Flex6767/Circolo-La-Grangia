a0eb5167192ba24f4e02c284c03ef69af536a555
Flex6767 <circololagrangia@gmail.com>
2024-12-01 17:23:49 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def submit():
 
     return "Iscrizione completata con successo!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
